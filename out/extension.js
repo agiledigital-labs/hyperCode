a5be9f1928944c2c3f4ff3fd58b787dc58a356b2
Sean Dawson <spdawson@agiledigital.com.au>
2021-03-22 08:54:09 +1100

    feat: got hypermerge to actually run

--- out/extension.js
+++ out/extension.js
@@ -1,29 +1,54 @@
 "use strict";
+/// <reference types="@seandawson/hypermerge/src/types/hyperswarm" />
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 const automerge = require("automerge");
-const fs_1 = require("fs");
 const vscode = require("vscode");
+const hypermerge_1 = require("@seandawson/hypermerge");
+const Hyperswarm = require("hyperswarm");
+const path = ".data";
+const repo = new hypermerge_1.Repo({ path });
+const swarm = Hyperswarm({ queue: { multiplex: true } });
+repo.addSwarm(swarm, { announce: true });
 function activate(context) {
-    console.log('Congratulations, your extension "a" is now active!');
-    let disposable = vscode.commands.registerCommand("a.helloWorld", () => {
-        var _a;
-        const activeText = ((_a = vscode === null || vscode === void 0 ? void 0 : vscode.window.activeTextEditor) === null || _a === void 0 ? void 0 : _a.document.fileName) || "hyper";
-        console.log(activeText);
-        let doc = automerge.from({ cards: [] });
-        let docStart = fs_1.readFileSync(activeText).toString();
-        vscode.workspace.onDidChangeTextDocument((event) => {
-            const text = event.contentChanges.map(t => t.text);
-            let docStart = automerge.change(doc, 'T1 typed', doc1 => {
-                // @ts-ignore
-                doc1.cards.push(text);
-            });
-            console.log(doc.cards);
-            console.log(event);
+    console.log('Congratulations, your extension "hypercode" is now active!');
+    context.subscriptions.push(vscode.commands.registerCommand("hypercode.start", () => {
+        const url = repo.create({
+            text: new automerge.Text(),
         });
-        vscode.window.showInformationMessage("url");
-    });
-    context.subscriptions.push(disposable);
+        vscode.window.showInformationMessage(`Url is [${url}]`);
+        vscode.workspace.onDidChangeTextDocument((event) => {
+            repo.change(url, (state) => {
+                event.contentChanges.forEach((change) => {
+                    var _a, _b, _c, _d;
+                    if (change.text === "") {
+                        (_b = (_a = state.text).deleteAt) === null || _b === void 0 ? void 0 : _b.call(_a, change.rangeOffset, change.rangeLength);
+                    }
+                    else {
+                        (_d = (_c = state.text).insertAt) === null || _d === void 0 ? void 0 : _d.call(_c, change.rangeOffset, ...Array.from(change.text));
+                    }
+                });
+            });
+        });
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand("hypercode.join", () => {
+        vscode.window
+            .showInputBox({
+            prompt: "Enter the HyperCode URL",
+        })
+            .then((url) => {
+            if (url === undefined) {
+                vscode.window.showWarningMessage("URL is required");
+                return;
+            }
+            // They seem to cast in the chat example, but it seems weird
+            repo.doc(url, (state) => {
+                var _a;
+                (_a = vscode.window.activeTextEditor) === null || _a === void 0 ? void 0 : _a.edit((editBuilder) => editBuilder.replace(new vscode.Position(0, 0), state.text.toString()));
+                state.text.toString();
+            });
+        });
+    }));
 }
 exports.activate = activate;
 // test method is called when your extension is deactivated
--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,72 @@
-import * as automerge from 'automerge';
-import { readFileSync } from "fs";
+/// <reference types="@seandawson/hypermerge/src/types/hyperswarm" />
+
+import * as automerge from "automerge";
 import * as vscode from "vscode";
 
+import { DocUrl, Repo } from "@seandawson/hypermerge";
+import * as Hyperswarm from "hyperswarm";
+
+const path = ".data";
+const repo = new Repo({ path });
+
+const swarm = Hyperswarm({ queue: { multiplex: true } });
+repo.addSwarm(swarm, { announce: true });
+
+type State = {
+  text: automerge.Text;
+};
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "a" is now active!');
-	
+  console.log('Congratulations, your extension "hypercode" is now active!');
 
-	let disposable = vscode.commands.registerCommand("a.helloWorld", () => {
-    const activeText =
-    vscode?.window.activeTextEditor?.document.fileName || "hyper";
-    console.log(activeText);
+  context.subscriptions.push(
+    vscode.commands.registerCommand("hypercode.start", () => {
+      const url = repo.create({
+        text: new automerge.Text(),
+      });
 
-	let doc = automerge.from({cards: []})
+      vscode.window.showInformationMessage(`Url is [${url}]`);
 
-
-
-    let docStart = readFileSync(activeText).toString();
-
-
-    vscode.workspace.onDidChangeTextDocument((event) => {
-
-		const text  =event.contentChanges.map(t => t.text)
-
-	let docStart = automerge.change(doc, 'T1 typed', doc1 =>{ 
-		// @ts-ignore
-		doc1.cards.push(text)
-	});
-
-	console.log(doc.cards)
-      console.log(event);
-    });
-
-    vscode.window.showInformationMessage("url");
-  });
-
-  context.subscriptions.push(disposable);
+      vscode.workspace.onDidChangeTextDocument((event) => {
+        repo.change(url, (state: State) => {
+          event.contentChanges.forEach((change) => {
+            if (change.text === "") {
+              state.text.deleteAt?.(change.rangeOffset, change.rangeLength);
+            } else {
+              state.text.insertAt?.(
+                change.rangeOffset,
+                ...Array.from(change.text)
+              );
+            }
+          });
+        });
+      });
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("hypercode.join", () => {
+      vscode.window
+        .showInputBox({
+          prompt: "Enter the HyperCode URL",
+        })
+        .then((url) => {
+          if (url === undefined) {
+            vscode.window.showWarningMessage("URL is required");
+            return;
+          }
+          // They seem to cast in the chat example, but it seems weird
+          repo.doc(url as DocUrl, (state: State) => {
+            vscode.window.activeTextEditor?.edit((editBuilder) =>
+              editBuilder.replace(
+                new vscode.Position(0, 0),
+                state.text.toString()
+              )
+            );
+            state.text.toString();
+          });
+        });
+    })
+  );
 }
 
 // test method is called when your extension is deactivated
